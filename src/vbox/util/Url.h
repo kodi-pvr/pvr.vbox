1881ae38b1fbb72f9bf817cade237bffdfd44d2b
Sam Stenvall <neggelandia@gmail.com>
2015-03-18 16:42:07 +0200

    fix wrong copyright in Url.h/cpp

--- src/vbox/util/Url.h
+++ src/vbox/util/Url.h
@@ -1,6 +1,7 @@
 #pragma once
 /*
-*      Copyright (C) 2015 Sam Stenvall
+*      Copyright (C) 2005-2013 Team XBMC
+*      http://xbmc.org
 *
 *  This Program is free software; you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
