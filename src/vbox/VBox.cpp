fd469e5cf925b0a69d1575f071632d6971bddcce
Sam Stenvall <neggelandia@gmail.com>
2015-04-08 15:48:08 +0300

    lower the version requirement to any 2.47 version since the versions don't follow semantic versioning

--- src/vbox/VBox.cpp
+++ src/vbox/VBox.cpp
@@ -38,7 +38,7 @@
 using namespace ADDON;
 using namespace vbox;
 
-const char * VBox::MINIMUM_SOFTWARE_VERSION = "2.47.105";
+const char * VBox::MINIMUM_SOFTWARE_VERSION = "2.47";
 
 VBox::VBox(const Settings &settings)
   : m_settings(settings), m_currentChannel("dummy", "dummy", "dummy", "dummy")
