707be50e12f6ffb9188e10febea9a34299740919
Alwin Esch <alwin.esch@web.de>
2020-04-27 18:16:23 +0200

    [API related] rename GetAddonCapabilities to GetCapabilities

--- src/client.cpp
+++ src/client.cpp
@@ -257,7 +257,7 @@ void OnPowerSavingDeactivated()
 {
 }
 
-PVR_ERROR GetAddonCapabilities(PVR_ADDON_CAPABILITIES* pCapabilities)
+PVR_ERROR GetCapabilities(PVR_ADDON_CAPABILITIES* pCapabilities)
 {
   pCapabilities->bSupportsTV = true;
   pCapabilities->bSupportsRadio = true;
